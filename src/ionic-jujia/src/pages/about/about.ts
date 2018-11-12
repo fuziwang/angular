36df28071352ddb1c012885029537c69f1c8200b
fuziwang <2622860598@qq.com>
2018-11-12 14:41:38 +0800

    update ionic-jujia

--- src/ionic-jujia/src/pages/about/about.ts
+++ src/ionic-jujia/src/pages/about/about.ts
@@ -1,5 +1,5 @@
-import { Component, ViewChild } from '@angular/core';
-import { NavController, Slides } from 'ionic-angular';
+import { Component} from '@angular/core';
+import { NavController} from 'ionic-angular';
 import {HttpClient} from '@angular/common/http';
 @Component({
   selector: 'page-about',
